ca7090fb5e8499eae6d1c658ac0a29f8f8d32ab1
Evans Owino <evansowino@hotmail.com>
2018-09-15 09:10:02 +0100

    Counter component hooked up to mobx

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 
-@observer class Counter extends React.Component {
+@observer
+class Counter extends React.Component {
   @observable count = 0;
 
-  handleDec = () => {
-    
-  };
-
-  handleInc = () => {
-    
-  };
+  handleDec = () => this.count--;
+  handleInc = () => this.count++;
 
   render() {
     return (
