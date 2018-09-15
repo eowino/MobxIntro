be0fbb3e4d4716a5771dac2df0f590dfd6f05cba
Evans Owino <evansowino@hotmail.com>
2018-09-15 09:08:06 +0100

    decorator support

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,27 +2,21 @@ import React from 'react';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 
-class Counter extends React.Component {
-  state = {
-    count: 0
-  };
+@observer class Counter extends React.Component {
+  @observable count = 0;
 
   handleDec = () => {
-    this.setState(({ count }) => ({
-      count: count - 1
-    }));
+    
   };
 
   handleInc = () => {
-    this.setState(({ count }) => ({
-      count: count + 1
-    }));
+    
   };
 
   render() {
     return (
       <div>
-        Count: {this.state.count}
+        Count: {this.count}
         <div>
           <button onClick={this.handleDec}>-</button>
           <button onClick={this.handleInc}>+</button>
